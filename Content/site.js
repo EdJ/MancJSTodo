3f89b587bd411e47c8d500b651a12cba54cb5c1f
Ed <edward.woodcock@googlemail.com>
2013-05-01 21:04:19 +0100

    Initial commit during the UG session

---
+++ Content/site.js
@@ -0,0 +1,45 @@
+﻿(function () {
+    var deleteLink = $('<a />').attr('href', '#').html('[x]').on('click', function () {
+        var parentLi = $(this).parents('li').first();
+        var id = parentLi.attr('id');
+
+        $.post('/delete', { id: id }, function (data) {
+            parentLi.remove();
+        });
+
+        return false;
+    });
+
+    var makeNewListItem = function (task) {
+        var newListItem = $('<li />').attr('id', task.id).html(task.todo).append(deleteLink.clone(true));
+
+        $('#todoList').append(newListItem);
+    };
+
+    $('#addTask').on('submit', function () {
+        var task = $(this).find('input[type=text]').val();
+
+        $.post('/add', { todo: task }, function (data) {
+            var item = JSON.parse(data);
+            makeNewListItem(item);
+        });
+
+        return false;
+    });
+
+    $(function () {
+        $.get('/getAll', function (data) {
+            var list = JSON.parse(data);
+
+            $.each(list, function (i, v) {
+                console.log(v);
+
+                if (v.done) {
+                    return;
+                }
+
+                makeNewListItem(v);
+            });
+        });
+    });
+})();
---
+++ init.js
@@ -0,0 +1,70 @@
+﻿var http = require('http');
+var express = require('express');
+var fs = require('fs');
+
+var app = express();
+var server = http.createServer(app);
+
+app.configure(function() {
+    app.use(express.static(__dirname + '/Content'));
+    app.use(express.bodyParser());
+});
+
+var returnFile = function (repsonse, fileName) {
+    fs.readFile(__dirname + '/Content/' + fileName, function(err, data) {
+        if (err) {
+            response.statusCode(500);
+            return;
+        }
+
+        response.write(data);
+    });
+};
+
+var addStatic = function(item) {
+    app.get('/', function (request, response) {
+        returnFile(response, item);
+    });
+};
+
+addStatic('index.html');
+addStatic('site.js');
+addStatic('site.css');
+
+var todoItems = [];
+var currentId = 0;
+
+app.post('/add', function (request, response) {
+    var newTask = {
+        id: currentId++,
+        added: new Date(),
+        todo: request.body.todo,
+        done: false
+    };
+
+    var responseText = JSON.stringify(newTask);
+    
+    todoItems.push(newTask);
+    response.send(responseText);
+});
+
+app.post('/delete', function(request, response) {
+    var idToDelete = request.body.id;
+    
+    for (var i = todoItems.length; i--;) {
+        var item = todoItems[i];
+        if (item.id == idToDelete) {
+            item.done = true;
+            break;
+        }
+    }
+
+    response.send({ success: true });
+});
+
+app.get('/getAll', function(request, response) {
+    response.send(JSON.stringify(todoItems));
+});
+
+
+server.listen(8080);
